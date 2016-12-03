15d1abd9a2c412cc26ecfcb468b9883c54206e36
Vildanix <Z1apvbslqa4mDz1DUO6G>
2016-12-03 12:17:18 +0100

    Added second part of day 3 solution. Fix separator for input file.

--- AdventOfCode2016/Day3.cs
+++ AdventOfCode2016/Day3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 // Advent of Code
 // Autor: Stanislav Tvrzník
@@ -23,8 +24,8 @@ namespace AdventOfCode2016 {
             StreamReader inputFileStream = new StreamReader("../../Resources/input-day3.txt");
             String inputFile = inputFileStream.ReadToEnd();
 
-            string[] coridorTriangles = inputFile.Split('\n');
-            //string[] coridorTriangles = testString.Split(new char[] { '.' });
+            string[] coridorTriangles = inputFile.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            //string[] coridorTriangles = testString.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
             // verify each triangle sequence
             string[] values;
@@ -49,8 +50,50 @@ namespace AdventOfCode2016 {
             Console.WriteLine("Valid triangle count: {0}", validTriangleCount);
             Console.ReadKey();
 
-            // part 2
 
+            // part 2
+            // triangles are in columns instead of rows
+            int columnCount = 3;
+
+            // verify triangles only if num of rows is divisible by 3
+            if (coridorTriangles.Length % 3 != 0) {
+                Console.WriteLine("Input file missing some triangles.");
+                Console.ReadKey();
+                return;
+            }
+
+            int[][] columns = new int[columnCount][];
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                columns[columnIndex] = new int[coridorTriangles.Length];
+            }
+            validTriangleCount = 0;
+
+            // build triangle arrays from columns
+            for (int rowIndex = 0; rowIndex < coridorTriangles.Length; rowIndex++) {
+                values = coridorTriangles[rowIndex].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // each triangle must have 3 sides
+                if (values.Length < columnCount)
+                    continue;
+
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                    columns[columnIndex][rowIndex] = int.Parse(values[columnIndex]);
+                }
+            }
+
+            // verify triangles in column
+            for (int rowIndex = 0; rowIndex < coridorTriangles.Length; rowIndex+=3) {
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                    if (isTriangle(columns[columnIndex].Skip(rowIndex).Take(3).ToArray())) {
+                        validTriangleCount++;
+                    }
+                }
+                
+            }
+
+            // print results
+            Console.WriteLine("Valid triangle count in columns: {0}", validTriangleCount);
+            Console.ReadKey();
         }
 
         private bool isTriangle(int[] values) {
