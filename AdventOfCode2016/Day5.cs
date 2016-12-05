45c1cddd7dd286b26420f79a4d87b661fb096378
Vildanix <Z1apvbslqa4mDz1DUO6G>
2016-12-05 14:44:17 +0100

    Added second part of day 5 - password "decryption" with cinematic effect

--- AdventOfCode2016/Day5.cs
+++ AdventOfCode2016/Day5.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
+using System.Text;
 
 // Advent of Code
 // Autor: Stanislav Tvrzník
@@ -23,14 +24,15 @@ namespace AdventOfCode2016 {
 
             //string puzzleInput = "abc";       // test string
             string puzzleInput = "abbhdwsy";
+            int passwordLength = 8;
 
             MD5 hashMD5 = MD5.Create();
             byte[] hashInputBuffer, hashOutput;
             uint passwordSize = 0, index = 0;
 
             // find all characters for door password
-            Console.WriteLine("Creating door key");
-            while (passwordSize < 8) {
+            Console.WriteLine("Creating first door key");
+            while (passwordSize < passwordLength) {
                 hashInputBuffer = System.Text.Encoding.ASCII.GetBytes(puzzleInput + index);
 
                 // create hash and test if first 5 hexadecimal values are zeros (using mask for last hexadecimal value)
@@ -42,7 +44,72 @@ namespace AdventOfCode2016 {
 
                 index++;
             }
+            Console.WriteLine("\nPress enter to enter first door code");
             Console.ReadKey();
+
+            // part 2
+            byte[] password = new byte[passwordLength];
+            byte[] foundPositions = new byte[passwordLength];
+            byte[] cinematic = new byte[passwordLength];
+            passwordSize = 0;
+            index = 0;
+
+            Console.WriteLine("\nSecond door");
+            Random random = new Random();           // only for cinematic effect
+            while (passwordSize < passwordLength) {
+                hashInputBuffer = System.Text.Encoding.ASCII.GetBytes(puzzleInput + index);
+
+                // create hash and test if first 5 hexadecimal values are zeros (using mask for last hexadecimal value)
+                hashOutput = hashMD5.ComputeHash(hashInputBuffer);
+                if (hashOutput[0] == 0x00 && hashOutput[1] == 0x00 && (hashOutput[2] & 0xf0) == 0x00) {
+                    // check if 6 hexa value is valid password position
+                    byte positionIndex = (byte)(hashOutput[2] & 0x0f);
+                    if (positionIndex < passwordLength) {
+
+                        // check if position is already filled and skip if value exists
+                        if (foundPositions[positionIndex] == 0xff) {
+                            index++;
+                            continue;
+                        }
+
+                        // save password symbol
+                        password[positionIndex] = (byte)(hashOutput[3] & 0xf0);
+                        foundPositions[positionIndex] = 0xff;
+
+                        // recalculate found positions
+                        passwordSize = 0;
+                        for (uint byteIndex = 0; byteIndex < foundPositions.Length; byteIndex++) {
+                            if (foundPositions[byteIndex] == 0xFF) {
+                                passwordSize++;
+                            }
+                        }
+                        
+                    }
+                }
+
+                // cinematic effect refresh once every 20000 hashes
+                if (index % 20000 == 0) {
+                    random.NextBytes(cinematic);
+                    for (int i = 0; i < passwordLength; i++) {
+                        cinematic[i] = (byte)(password[i] | (cinematic[i] & ~foundPositions[i]));
+                    }
+
+                    Console.Write("\r{0}", getPasswordAsString(cinematic));
+                }
+
+                index++;
+            }
+            Console.WriteLine("\rPassword: {0}", getPasswordAsString(password));
+            Console.ReadKey();
+        }
+
+        private string getPasswordAsString(byte[] bytes) {
+            StringBuilder byteString = new StringBuilder(bytes.Length);
+            for(uint byteIndex = 0; byteIndex < bytes.Length; byteIndex++) {
+                byteString.AppendFormat("{0:X}", (bytes[byteIndex] & 0xf0) >> 4);
+            }
+                
+            return byteString.ToString();
         }
 
     }
