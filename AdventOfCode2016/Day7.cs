929a711741d7d839ce758aae80f07aa3f3c9dd4f
Vildanix <Z1apvbslqa4mDz1DUO6G>
2016-12-07 12:59:06 +0100

    Added second part of day 7

--- AdventOfCode2016/Day7.cs
+++ AdventOfCode2016/Day7.cs
@@ -12,28 +12,33 @@ namespace AdventOfCode2016 {
     class Day7 {
 
         public void createSolution() {
-            // part 1
             /*
              * An IP supports TLS if it has an Autonomous Bridge Bypass Annotation, or ABBA.
              * An ABBA is any four-character sequence which consists of a pair of two different characters
              * followed by the reverse of that pair, such as xyyx or abba. However, the IP also must not have an ABBA
              * within any hypernet sequences, which are contained by square brackets.
+             * 
+             * TLS (transport-layer snooping)
+             * SSL (super-secret listening)
              */
 
             // test string
             //string inputAddr = "abba[mnop]qrst\nabcd[bddb]xyyx\naaaa[qwer]tyui\nioxxoj[asdfgh]zxcvbn";
-            
+
             // load and prepare file containing input
             string inputAddr = File.ReadAllText("../../Resources/input-day7.txt");
             string[] ipv7Addr = inputAddr.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
 
             int tlsCount = 0;
+            int sslCount = 0;
             foreach(string addr in ipv7Addr) {
-                // verify address TLS
-                tlsCount += isAddressTLS(addr) ? 1 : 0;
+                // verify address TLS and SSL
+                tlsCount += isAddressTLS(addr) ? 1 : 0; // part 1
+                sslCount += isAddressSSL(addr) ? 1 : 0; // part 2
             }
 
             Console.WriteLine("TLS supporting IP Count: {0}", tlsCount);
+            Console.WriteLine("SSL supporting IP Count: {0}", sslCount);
 
             Console.ReadKey();
         }
@@ -69,6 +74,36 @@ namespace AdventOfCode2016 {
             }
 
             return symetryFound;
+        }
+
+        private bool isAddressSSL(string address) {
+            bool isBracket = false;
+            char[] addrChars = address.ToCharArray();
+            for (int i = 2; i < addrChars.Length; i++) {
+                // switch allowed symetry
+                if (addrChars[i] == '[') {
+                    isBracket = true;
+                    i += 2;
+                    continue;
+                }
+
+                if (addrChars[i] == ']') {
+                    isBracket = false;
+                    i += 2;
+                    continue;
+                }
+
+                // find sequence ABA outside brackets and then sequence BAB inside bracket
+                if (!isBracket && addrChars[i - 2] == addrChars[i] && addrChars[i] != addrChars[i - 1]) {
+                    Regex regex = new Regex(@"\[[a-z]*" + addrChars[i - 1] + addrChars[i] + addrChars[i - 1] + "[a-z]*\\]");
+                    Match match = regex.Match(address);
+                    if (match.Success) {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
     }
