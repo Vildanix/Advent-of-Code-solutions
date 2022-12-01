ac850831501160f5c44d27747cd8981c6b7b1fd1
Tvrzník Stanislav <stanislav.tvrznik@ibrconsulting.cz>
2022-12-01 10:27:17 +0100

    Modify solution to generate second half for first day

--- AdventOfCode2022/Tasks/Day1.cs
+++ AdventOfCode2022/Tasks/Day1.cs
@@ -24,46 +24,51 @@ namespace AdventOfCode2022.Tasks
              *  https://adventofcode.com/2022/day/1
              */
 
-            var solution = SolveInput(inputFile);
+            var solution = SolveInput(inputFile, 1);
             Console.WriteLine($"Elf carrying the most Calories is: {solution.Item1} with {solution.Item2} Calories");
         }
 
         protected override void PrintSecondSolution(string inputFile)
         {
             /* 
+             * Find first three elf with most Calories
              */
-
-            Console.WriteLine($"Solution 2: ");
+            int numberOfElves = 3;
+            var solution = SolveInput(inputFile, numberOfElves);
+            Console.WriteLine($"Top {numberOfElves} elves carrying the most Calories are: {solution.Item1} with {solution.Item2} Calories");
         }
 
-        private (int, int) SolveInput(string input)
+        private (string, int) SolveInput(string input, int elfCount)
         {
             var readings = ParseInput(input);
-            var maxValue = readings.Max();
+            readings.Sort((a, b) => b.Item2 - a.Item2);
+            var maxValues = readings.Take(elfCount);
 
-            return (readings.IndexOf(maxValue) + 1, maxValue);
+            return (string.Join(", ", maxValues.Select(v => v.Item1.ToString())), maxValues.Sum(v => v.Item2));
         }
 
-        private List<int> ParseInput(string input)
+        private List<(int, int)> ParseInput(string input)
         {
             return GetCaloriesForElfs(SeparateLines(input, StringSplitOptions.None)).ToList();
         }
 
-        public IEnumerable<int> GetCaloriesForElfs(IEnumerable<string> input)
+        public IEnumerable<(int, int)> GetCaloriesForElfs(IEnumerable<string> input)
         {
             var elfCalories = 0;
+            var elfNumber = 1;
             foreach (var item in input)
             {
                 if (string.IsNullOrEmpty(item))
                 {
-                    yield return elfCalories;
+                    yield return (elfNumber, elfCalories);
                     elfCalories = 0;
+                    elfNumber++;
                 }
                 else 
                     elfCalories += int.Parse(item);
             }
 
-            yield return elfCalories;
+            yield return (elfNumber, elfCalories);
         }
     }
 }
